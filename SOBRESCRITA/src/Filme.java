217c1fe2af438eb5c94f310bff66c2ce41de3abf
M0tn4ph-1g1s <62472454+M0tn4ph-1g1s@users.noreply.github.com>
2021-09-13 15:44:57 -0300

    Delete SOBRESCRITA directory

--- SOBRESCRITA/src/Filme.java
+++
@@ -1,9 +0,0 @@
-public class Filme {
-    public void alugarFilme(int dias){
-          if ((dias > 0) && (dias <= 5)){
-              System.out.println("Aluguel feito.");
-          }else{
-              System.out.println("Não é possível alugar um filme por menos de 0 dias ou mais de 5 dias.");
-          }
-    }
-}
--- SOBRESCRITA/src/FilmeSobrescrita.java
+++
@@ -1,9 +0,0 @@
-public class filmeSobrescrita{
-    public void main (String[] args){
-        Filme filme = new Filme();
-        Filme24Horas filme2 = new Filme();
-        
-        filme.alugarFilme(3);
-        filme2.alugarFilme(5);
-    }
-}
